5211ec9232dea7d4b8a962427dda98ec00966593
alessandraaguiluz <alessandraaguiluz0@gmail.com>
2025-03-13 19:39:48 -0600

    create category.php

---
+++ app/Models/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+        ];
+    }
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+}
